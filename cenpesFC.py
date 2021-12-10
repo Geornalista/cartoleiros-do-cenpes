41ed8b5be23deb41e2820cbf05c67f7b0ae6f3dd
Geornalista <83824843+Geornalista@users.noreply.github.com>
2021-12-10 17:29:14 -0300

    Update cenpesFC.py

--- cenpesFC.py
+++ cenpesFC.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from matplotlib.ticker import MaxNLocator
 
 #================================
-rodada_atual = 37
+rodada_atual = 38
 #================================
 
 # FUNÇÕES ====================
